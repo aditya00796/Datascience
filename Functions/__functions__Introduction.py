51f5b4b0f38cdf06e8c032adc411e5d620746190
unknown <Deepika>
2023-05-12 00:36:23 +0800

    initial commit

---
+++ Functions/__functions__Introduction.py
@@ -0,0 +1,44 @@
+"""
+
+(1) Definition of Function:
+It performs a task which is re-usable contains block of programming statements
+The keyword is 'def'
+"""
+
+#Syntax
+# def function_name(parameters) :
+#     """Write String"""
+#     statement1
+#     statement2
+#     statement3
+#     ...
+#     return [expr]
+
+
+# Calculator
+def calculator(a,b):
+    """Calculator"""
+    add = a+b
+    sub = a-b
+    return sub
+
+res = calculator(12,6)
+print("Substraction :", res)
+
+# Printing string
+# Return keyword returns the output of the function
+def username(str) :
+    """Printing name"""
+    user = str
+    return user;
+res = username("Deepika")
+print(res)
+
+
+#
+
+
+
+
+
+
